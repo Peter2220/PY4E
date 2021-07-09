4eedcf7174caca8465448f2bb32dbf2bba378a26
Peter2220 <56364669+Peter2220@users.noreply.github.com>
2021-07-09 23:35:06 +0200

    First commit

---
+++ Day 1.py
@@ -0,0 +1,42 @@
+"""
+text = "While this is"
+for character in text:
+    print(character)
+"""
+
+# Exercise 1
+"""
+name = input("Enter your name:\n\n")
+
+print("\nHello", name)
+"""
+
+# Exercise 2
+"""
+hours = float(input("Enter Hours: "))
+rate = float(input("Enter Rate: "))
+pay = hours * rate
+print("Pay:", pay)
+"""
+
+# Exercise 3
+"""
+width = 17
+height = 12.0
+
+print(width//2)    # Output = 8
+print(width/2.0)   # Output = 8.5
+print(height/3)    # Output = 4.0
+print(1+2*5)       # Output = 11
+"""
+
+# Exercise 4  Temperature conversion (Celsius to Fahrenheit)
+# Celsius to Fahrenheit formula:      °F =°C * 1.8000 + 32.00
+celsius = int(input("Enter Celsius temperature: "))
+fahrenheit = celsius * 1.8000 + 32.00
+print("\n\nFahrenheit temperature is:", fahrenheit)
+
+"""
+30°C= 86°F
+33°C= 91.4°F
+"""
